47f7628cde190733ae6e0364a55875a02a767e91
SimonDORNIER <60660700+SimonDORNIER@users.noreply.github.com>
2026-09-13 18:54:22 +0200

    Launch MegaMobile V0.6 game layer

--- app/src/main/java/com/megamobile/game/MainActivity.java
+++ app/src/main/java/com/megamobile/game/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends Activity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         immersive();
-        gameView = new GameViewFinal(this);
+        gameView = new GameViewV6(this);
         setContentView(gameView);
         UpdateManager.checkAndUpdate(this);
     }
