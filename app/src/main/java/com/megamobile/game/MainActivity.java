296eecfd4d3d8519453f065db7f8d20671f06806
SimonDORNIER <60660700+SimonDORNIER@users.noreply.github.com>
2026-09-13 19:49:42 +0200

    Use final V0.7 gameplay wrapper

--- app/src/main/java/com/megamobile/game/MainActivity.java
+++ app/src/main/java/com/megamobile/game/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends Activity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         immersive();
-        gameView = new GameViewV7(this);
+        gameView = new GameViewV7Final(this);
         setContentView(gameView);
         UpdateManager.checkAndUpdate(this);
     }
