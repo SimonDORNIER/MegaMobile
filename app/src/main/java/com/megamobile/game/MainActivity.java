f4d968936f9043b3fca51b6cf1996d19377aa6b9
SimonDORNIER <60660700+SimonDORNIER@users.noreply.github.com>
2026-09-16 19:34:33 +0200

    Use MegaMobile V1.0 gameplay view

--- app/src/main/java/com/megamobile/game/MainActivity.java
+++ app/src/main/java/com/megamobile/game/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends Activity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         immersive();
-        gameView = new GameViewV8(this);
+        gameView = new GameViewV10(this);
         setContentView(gameView);
         UpdateManager.checkAndUpdate(this);
     }
